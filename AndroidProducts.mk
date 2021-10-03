818a3c25249a39c10726492d4959637794ffdd78
Rahul Singh <demogorgon@pop-os.localdomain>
2021-10-03 12:11:13 +0530

    Raphael: Initial Aex Setup
    Raphael: Setup Aex

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_raphael.mk
+    $(LOCAL_DIR)/aosp_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_raphael-user \
-    nad_raphael-userdebug \
-    nad_raphael-eng
+    aosp_raphael-user \
+    aosp_raphael-userdebug \
+    aosp_raphael-eng
--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -12,21 +12,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit some common Aex stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := nad_raphael
+PRODUCT_NAME := aosp_raphael
 
 PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-NAD_BUILD_TYPE := OFFICIAL
-#USE_GAPPS ?= true
-#TARGET_GAPPS_ARCH := arm64
 TARGET_FOD_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true
